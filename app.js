c9e03144f8057ea6ea615718bd71303eae088c69
AirV <83022834+herveNkb@users.noreply.github.com>
2022-08-28 11:35:02 +0200

    Changing old citations per more beautiful citations

--- app.js
+++ app.js
@@ -13,33 +13,50 @@ let citations = [
         "La vie est un mystère qu'il faut vivre, et non un problème à résoudre.",
         'Gandhi',
     ],
-    ['Le plus grand risque est de ne prendre aucun risque.', 'Mark Zuckerberg'],
-    ['Méritez votre statut de leader chaque jour.', 'Mickael Jordan'],
-    ['Soyez le changement que vous voulez voir dans le monde.', 'Gandhi'],
     [
-        'A chaque fois que vous vous retrouvez du même côté que la majorité, il est temps de prendre du recul, et de réfléchir.',
-        'Mark Twain',
+        'Le plus grand risque est de ne prendre aucun risque.',
+        'Mark Zuckerberg'
+    ],
+    [
+        'Les mots d\'une phrase ou d\'un vers sont les traces, les cicatrices des sentiments de l\'auteur.',
+        'Fabrice Luchini'
+    ],
+    [
+        'Écrire, c\'est se cacher derrière les mots tout en se mettant à nu.',
+        'Claudine Paquet'
+    ],
+    [
+        'Le plus dur ce sont ces doutes, ces intermittences, ces vides si complets que je me demande parfois si tout ce qui est arrivé n\'est pas un rêve.\n' +
+        'Et quand la joie revient dans sa plénitude, j\'ai bien honte alors d\'avoir eu la lâcheté de ne plus y croire.',
+        'Simone de Beauvoir',
     ],
     [
         'Seulement ceux qui prendront le risque d’aller trop loin découvriront jusqu’où on peut aller.',
         'T.S Elliot',
     ],
-    ['Le succès c’est tomber sept fois, se relever huit.', 'Proverbe japonais'],
     [
-        'Dans vingt ans vous serez plus déçus par les choses que vous n’avez pas faites que par celles que vous avez faites. Alors sortez des sentiers battus. Mettez les voiles. Explorez. Rêvez. Découvrez.',
-        'Mark Twain',
+        'Le succès c’est tomber sept fois, se relever huit.',
+        'Proverbe japonais'
+    ],
+    [
+        'Aujourd\'hui, je suis convaincue que personne ne perd personne, parce que personne ne\n' +
+        'possède personne. C\'est cela la véritable expérience de la liberté: avoir la chose la\n' +
+        'plus importante au monde, sans la posséder.',
+        'Paulo Coehlo',
     ],
     [
         'Si vous attendez pour agir, tout ce que vous gagnerez, avec le temps, c’est de l’âge.',
         'Brian Tracy',
     ],
     [
-        'Quand on concentre son attention sur un seul projet, l’esprit suggère constamment des idées et des améliorations qui lui échapperaient s’il était occupé avec plusieurs projets en même temps.',
-        'P.T. Barnum',
+        'La gentillesse dans les mots suscite la confiance.\n' +
+        ' La gentillesse dans la pensée crée la profondeur.\n' +
+        'La gentillesse dans les actes engendre l\'amour.',
+        'Lao-Tseu',
     ],
     [
-        'Se dédier à faire tout ce que l’on peut pour aider les autres à obtenir ce qu’ils veulent, c’est la clé du succès.',
-        'Brian Sher',
+        'Aimer la littérature, c\'est être persuadé qu\'il y a une phrase écrite qui nous redonnera le goût de vivre.',
+        'Georges Perros',
     ],
     [
         'Si vous pensez que vous êtes trop petit pour avoir de l’impact, essayez d’aller au lit avec un moustique.',
@@ -49,7 +66,10 @@ let citations = [
         'Ne jugez pas chaque jour sur ce que vous récoltez, mais sur les graines que vous semez.',
         'Robert Louis Stevenson',
     ],
-    ['L’action est la clé fondamentale de tout succès.', 'Pablo Picasso'],
+    [
+        'L’action est la clé fondamentale de tout succès.',
+        'Pablo Picasso'
+    ],
     [
         'Le succès, c’est se promener d’échecs en échecs tout en restant motivé.',
         'Winston Churchill',
@@ -59,16 +79,17 @@ let citations = [
         'Robert T. Kiyosaki',
     ],
     [
-        'Ne vous découragez pas, c’est souvent la dernière clef du trousseau qui ouvre la porte.',
-        'Zig Ziglar',
+        'Il y aura toujours une plume pour écrire le futur, mais jamais une gomme pour effacer le passé. ',
+        'Anonyme',
     ],
     [
-        'Pour gagner votre vie, apprenez à l’école. Pour gagner une fortune, apprenez par vous-même.',
-        'Brian Tracy',
+        'Il semble qu\'il existe dans le cerveau une zone tout à fait spécifique qu\'on pourrait appeler la mémoire poétique \n'+
+        'et qui enregistre ce qui nous a charmé, ce qui nous a émus, ce qui donne à notre vie sa beauté.',
+        'Milan Kundera',
     ],
     [
-        'Les gagnants trouvent des moyens, les perdants des excuses…',
-        'F. D. Roosevelt',
+        'Ce ne sont pas les autres qui nous infligent les pires déceptions, mais le choc entre la réalité et les emballements de notre imagination !',
+        'Hélène Grémillon',
     ],
     [
         'Vous n’êtes jamais trop vieux pour vous fixer de nouveaux buts, ou rendre vos rêves réalité.',
@@ -77,6 +98,14 @@ let citations = [
     [
         'Un pessimiste voit la difficulté dans chaque opportunité. Un optimiste voit une opportunité dans chaque difficulté.',
         'Winston Churchill',
+    ],
+    [
+        'L\'attention est la forme la plus rare et la plus pure de la générosité !',
+        'Simone Weil'
+    ],
+    [
+        'Sans la littérature on ne saurait pas ce que pense un homme lorsqu\'il est seul.',
+        'Georges Perros'
     ],
 ];
 
